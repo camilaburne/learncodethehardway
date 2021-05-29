71eb113f216d5d26a3062e4f877a1c08046754f1
camilaburne <camilaburne@gmail.com>
2021-05-29 14:21:59 -0500

    Added files from Ex 46, a project skeleton

---
+++ python/skeleton/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+config = {
+    'description': 'My project',
+    'author': 'My name',
+    'url': '',
+    'download_url': '',
+    'author_email': '',
+    'version': '',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+    }
